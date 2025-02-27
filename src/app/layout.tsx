cedae1a080f3a7e3a02f63d08d3eaeecb89d67d2
Wesley Alves <wesandradealves@gmail.com>
2025-02-27 02:13:42 -0300

    chore(scss): mudança de prioridade de código para reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
                 </AnimatePresence>
               </StyledJsxRegistry>
             </Suspense>
-          <GlobalStyle />
+            <GlobalStyle />
         </ThemeProvider>
       </body>
     </html>
