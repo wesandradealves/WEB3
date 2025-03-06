9abeb63055c6e9aedabf85a01892cb06ca7b1cef
Wesley Alves <wesandradealves@gmail.com>
2025-03-06 19:43:15 -0300

    feat(navigation): implementação do menu mobile, correções de bugs, refatoração de código e menu suspenso
    - Implementação do menu mobile com animação de transição
    - Adição de efeito de desfoque no fundo do menu mobile
    - Correções de bugs relacionados à navegação
    - Refatoração do código para melhorar a legibilidade e manutenção
    - Implementação de menu suspenso para itens de navegação

--- src/components/navigation/typo.ts
+++ src/components/navigation/typo.ts
@@ -3,11 +3,13 @@ export interface navigation {
     url: string;
     type?: string;
     btnClass?: string;
+    below?: navigation[];
     className?: string;
 }
 
 export interface Props {
     className?: string;
+    mobile?: boolean;
     gap?: number;
     data: navigation[];
     ListClassName?: string;
