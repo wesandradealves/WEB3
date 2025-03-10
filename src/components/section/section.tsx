b8528294b1b627613318e390c8f8b46eb7f25e4b
Wesley Alves <wesandradealves@gmail.com>
2025-03-10 10:26:10 -0300

    feat: adicionar propriedade barstitle aos títulos do componente Section
    - Adicionada a propriedade barstitle ao componente Section para permitir a exibição de uma barra nos títulos.
    - Atualização do componente Home para utilizar a nova propriedade barstitle no componente Section.

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -3,7 +3,7 @@
 import { Container, SectionHeader, Subtitle, Title } from './styles';
 import { Props } from './typo';
 
-export default function Section({ children, title, subtitle, className, id, background, backgroundcolor }: Props) {
+export default function Section({ barstitle, children, title, subtitle, className, id, background, backgroundcolor }: Props) {
   return (
     <Container 
         id={id ?? ""}
@@ -11,8 +11,9 @@ export default function Section({ children, title, subtitle, className, id, back
         backgroundcolor={backgroundcolor ?? ""}
         className={`w-full m-auto overflow-hidden relative ${ className ?? "" }`}>
         <div className='container relative m-auto flex flex-col pt-11 pb-11 gap-7'>
+            {barstitle}
             <SectionHeader className='flex flex-col justify-center items-center text-center w-full gap-7'>
-                {title && <Title className='text-center' dangerouslySetInnerHTML={{ __html: title }} />}
+                {title && <Title barstitle={barstitle} className='text-center' dangerouslySetInnerHTML={{ __html: title }} />}
                 {subtitle && <Subtitle className='text-center' dangerouslySetInnerHTML={{ __html: subtitle }} />}
             </SectionHeader>
             {children}
