6361abc609bb9d46945da3d26bee7598e457f860
Wesley Alves <wesandradealves@gmail.com>
2025-03-11 20:55:54 -0300

    feature(section): novas propriedades de background adicionadas como attachment, size e position para flexibilizar o controle do fundo das sessoes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,11 +15,18 @@ export default function Home() {
       }}/>
 
       <Section 
+        backgroundattachment='fixed'
+        backgroundimage='/img/copie-de-onde-o-dinheiro-se-transforma-em-ouro-3.png'
         title='Uma <span><b>Nova Era</b></span> de Prosperidade <b>Compartilhada</b>' >
-        <>Conteudo</>
+        <p className='text-center'>O Metaverso e as criptomoedas estão redefinindo como negociamos, criamos e nos conectamos. <b>Na BDM Digital</b>, desenvolvemos a primeira <span>blockchain brasileira</span> com validação em segundos, 
+        usando um <b>Proof-of-Stake aprimorado</b>, para garantir transações rápidas e seguras. <br/><br/>Combinada à <span><b>Dourado.cash</b></span>, nossa plataforma de pagamentos, oferecemos uma ponte entre o mundo físico e o digital, onde todos podem usufruir de liberdade financeira, seja em moeda social (BDM) ou em criptoativos globais.</p>
       </Section>
 
       <Section 
+        backgroundsize='auto'
+        backgroundposition='center center'
+        backgroundattachment='fixed'
+        backgroundimage='/img/tecnologia-que-transforma-realidades.png'
         title='Tecnologia que <span><b>Transforma Realidades</b></span>' >
         <>Conteudo</>
       </Section>
@@ -31,6 +38,7 @@ export default function Home() {
       </Section>
 
       <Section 
+        background='linear-gradient(180deg, #161516 54.74%, #777777 91.02%)'
         title='<span><b>DBM Digital:</b></span>  <b>Sua Ponte para o Metaverso</b>' >
         <>Conteudo</>
       </Section>
--- src/components/section/typo.ts
+++ src/components/section/typo.ts
@@ -1,8 +1,12 @@
 export interface Props {
     title: string;
     subtitle?: string;
+    backgroundimage?:string;
     background?: string;
     backgroundcolor?: string;
+    backgroundsize?: string;
+    backgroundposition?: string;
+    backgroundattachment?: string;
     className?: string;
     id?: string;
     children: React.ReactNode;
