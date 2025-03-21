082366a7e1ccd98d6fa5f32cba123c4bc9c7f039
Wesley Alves <wesandradealves@gmail.com>
2025-03-21 13:17:29 -0300

    chore(clientes): inserçao do conteudo grid de clientes e fix nas margens do fim de cada pagina criada

---
+++ src/app/clientes/style.tsx
@@ -0,0 +1,23 @@
+import { pxToRem } from '@/utils';
+import styled from 'styled-components';
+
+export const Clients = styled.ul`
+
+`;
+
+export const Client = styled.li`
+
+`;
+
+export const Title = styled.h3`
+    font-size: ${pxToRem(18)};
+`;
+
+export const Taglist = styled.ul`
+
+`;
+
+export const Tag = styled.li`
+    font-size: ${pxToRem(12)};
+    color: black;
+`;
