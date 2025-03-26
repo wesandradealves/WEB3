e67e7071ed8e8f259790ddb270af7a7494e987f3
PauloMats <pm.mats98@gmail.com>
2025-03-26 13:40:24 -0300

    update: atualização de informações das notícias

--- src/context/media.tsx
+++ src/context/media.tsx
@@ -24,19 +24,31 @@ export const MediaProvider = ({ children }: { children: ReactNode }) => {
             id: 0,
             title: 'Após ultrapassar InfoMoney, Exame e InvestNews, BM&C News é o maior canal brasileiro sobre notícias do mercado',
             body: 'No segmento de finanças, canal deixou para trás seus principais concorrentes como: InfoMoney (29 milhões de visualizações) Exame (33 milhões) e InvestNews, de Dony De Nuccio e Samy Dana, que registravam, na segunda-feira (7) 35 milhões de visualizações.',
-            thumbnail: '/img/thumb.png'
+            thumbnail: '/img/thumb.png',
+            date: '2022-04-20',
+            author: 'BM&C News',
+            category: 'Mercados',
+            readTime: '5 min de leitura'
         },
         {
             id: 1,
             title: 'Na vanguarda do setor, BDM, a "moeda de zigurats", será beneficiada com nova "lei das criptomoedas"',
             body: 'Com diversos estabelecimentos e serviços utilizando BDM Digital como forma de pagamento, o giro da economia local gera prosperidade financeira para a população.',
-            thumbnail: '/img/thumb-2.png'
+            thumbnail: '/img/thumb-2.png',
+            date: '2022-12-22',
+            author: 'Correio do Estado',
+            category: 'Ativos Digitais',
+            readTime: '5 min de leitura'
         },
         {
             id: 2,
             title: 'BDM Digital: conheça a moeda que está mudando a vida de muitas pessoas',
             body: 'Com diversos estabelecimentos e serviços utilizando BDM Digital como forma de pagamento, o giro da economia local gera prosperidade financeira para a população.',
-            thumbnail: '/img/thumb-3.png'
+            thumbnail: '/img/thumb-3.png',
+            date: '2021-09-06',
+            author: 'G1 - Globo',
+            category: 'Mercados',
+            readTime: '5 min de leitura'
         }
     ]);
 
