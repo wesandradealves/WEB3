88b1cbcbf6d8c5507782b7e78a6e665162f8e29a
Wesley Alves <wesandradealves@gmail.com>
2025-04-04 19:19:33 -0300

    chore(mediascroll): estilo box e refatoraçao do loop de imagens

--- src/components/box/box.tsx
+++ src/components/box/box.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { Container } from './styles';
+import { BoxContainer } from './styles';
 import { Props } from './typo';
 import classNames from 'classnames';
 
 export default function Box(Props: Props) {
   return (
-    <Container 
+    <BoxContainer 
       className={classNames(`box gap-${Props.gap ?? 3} flex-col lg:flex-row flex justify-${Props.justify ?? 'between'} items-${Props.alignment ?? 'center'} ${Props.classname}`, {
         'flex-wrap': Props.wrap
       })}>
       {Props.children}
-    </Container>
+    </BoxContainer>
   );
 }
