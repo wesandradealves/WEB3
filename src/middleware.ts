6730d4c1b40511b4ee0338fd2ae97bb7e0b2ac5c
Wesley Andrade (WES) <wesandradealves@gmail.com>
2025-05-26 18:27:02 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,23 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/') {
+  const { pathname, origin } = request.nextUrl;
+
+  if (pathname === '/') {
     return NextResponse.redirect(new URL('/home', request.url));
   }
+
+  if (pathname.startsWith('/wp-content/uploads/')) {
+    const newUrl = new URL(request.url);
+    newUrl.hostname = 'dev-bdm.dourado.cash';
+    newUrl.protocol = 'https:';
+    newUrl.port = '8000';
+    return NextResponse.rewrite(newUrl);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/', 
+  matcher: ['/', '/wp-content/uploads/:path*'],
 };
