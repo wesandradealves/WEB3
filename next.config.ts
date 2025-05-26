a61a5135be6fa6578c060ac37a804f1a95e33830
Wesley Andrade (WES) <wesandradealves@gmail.com>
2025-05-26 18:42:32 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/wp-content/uploads/:path*',
+        destination: 'https://dev-bdm.dourado.cash:8000/wp-content/uploads/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
