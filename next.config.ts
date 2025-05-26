6cbd07ba75422888be7ad4011ebed71814bb3414
Wesley Andrade (WES) <wesandradealves@gmail.com>
2025-05-26 18:00:26 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/images/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'dev-bdm.dourado.cash',
+          },
+        ],
+        destination: 'http://177.71.161.215:8000/images/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
