165d8aa00c9057014d0922792267902c15a0c6c6
Wesley Andrade (WES) <wesandradealves@gmail.com>
2025-05-27 19:19:39 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const isHml = process.env.NEXT_PUBLIC_ENVIROMENT === 'hml' || process.env.NODE_ENV === 'production';
+
+const baseURL =
+  isHml
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    : `${process.env.NEXT_PUBLIC_API_URL_DEV}${process.env.NEXT_PUBLIC_API_BASE_URL}`;
+
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_ENVIROMENT === 'hml' ? process.env.NEXT_PUBLIC_API_URL_HML : process.env.NEXT_PUBLIC_API_URL_DEV}${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+  baseURL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
