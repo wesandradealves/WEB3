1ccf1f5cedf732d350722be96f301ae5ea5317a2
Wesley Alves <wesandradealves@gmail.com>
2025-06-13 13:58:35 -0300

    Correçao de textos e ui/ux mobile

--- src/components/firebasegametrics/index.tsx
+++ src/components/firebasegametrics/index.tsx
@@ -85,8 +85,8 @@ const Firebasegametrics = (props: Props | GABlockProps) => {
                                 {mergedProps?.title && (
                                     <Title
                                         barstitle={mergedProps?.barstitle}
-                                        className={classNames(`text-center relative text-3xl lg:text-5xl`, {
-                                            'pt-2 pb-2 ps-[150px] pe-[150px]': !!mergedProps?.barstitle
+                                        className={classNames(`text-center relative text-2xl lg:text-5xl`, {
+                                            'pt-2 pb-2 lg:ps-[150px] lg:pe-[150px]': !!mergedProps?.barstitle
                                         })}
                                         dangerouslySetInnerHTML={{ __html: mergedProps.title }}
                                     />
