00bf91d73d54b901bd06db43ccc33f54601e9bfa
PauloMats <pm.mats98@gmail.com>
2025-07-22 16:55:32 -0300

    feat(styles.tsx): ajusta estilos

--- src/components/FeatureCard/styles.tsx
+++ src/components/FeatureCard/styles.tsx
@@ -1,23 +1,41 @@
 import styled from 'styled-components';
+import { pxToRem } from '@/utils';
+import { colors } from '@/assets/scss/colors';
+
+interface VerticalLineProps {
+  lineColor?: 'gold' | 'darkGold';
+}
 
 export const StyledFeatureCardContainer = styled.div`
   position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 5.5rem; /* mt-6 */
-  padding: 1.5rem; /* p-6 */
-  background-color: transparent;
+  background-color: rgba(255, 255, 255, 0.05);
+  height: 100%;
+  padding: ${pxToRem(32)}; 
+  padding-left: ${pxToRem(24)}; 
+  border-radius: ${pxToRem(12)}; 
 `;
 
-export const VerticalLine = styled.div<{ lineColor: 'gold' | 'darkGold' }>`
+export const VerticalLine = styled.div<VerticalLineProps>`
   position: absolute;
   left: 0;
   top: 0;
   bottom: 0;
-  margin-top: 1.5rem;
-  width: 4px;
+  width: ${pxToRem(4)};
   background-color: ${({ lineColor }) =>
-    lineColor === 'gold' ? '#FFD700' : '#FFFFFF'};
+    lineColor === 'gold' ? colors.primary.bdm3 : colors.primary.bdm1};
+  
+  border-radius: ${pxToRem(4)};
 `;
 
+export const Title = styled.h3`
+  font-weight: bold;
+  color: ${colors.primary.bdm4}; 
+  margin-bottom: ${pxToRem(8)};
+  font-size: ${pxToRem(24)};
+`;
+
+export const Description = styled.p`
+  color: ${colors.primary.bdm10}; 
+  line-height: 1.6;
+  font-size: ${pxToRem(16)};
+`;
